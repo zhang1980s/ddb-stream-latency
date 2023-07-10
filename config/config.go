bd63c2986aaf90f8acf4ce6efc5f39b57195fcd7
Zhang Zhe <zzhe@amazon.com>
2023-07-10 07:05:02 +0000

    Adding config.go

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	RepoName                = "zhang1980s/ddb-stream-latency"
+	RepoBranch              = "main"
+	ConnectionArn           = "arn:aws:codestar-connections:ap-southeast-1:894855526703:connection/f7f379e6-e65f-4886-8629-54f739817ad4"
+	PipelineName            = "StackPipeline"
+	TimerTableReadCapacity  = 1
+	TimerTableWriteCapacity = 1
+)
